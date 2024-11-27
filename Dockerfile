55470796c7ba78ff8ae5f5ca19bbcac7df2a1a17
ahyeon-github <ahyeon31@sookmyung.ac.kr>
2024-11-27 16:15:02 +0900

    fix : build 에러 해결 (test 환경 분리

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# 1. Base image
+FROM openjdk:17-jdk-slim
+
+# 2. Add a volume to hold logs
+VOLUME /tmp
+
+# 3. Copy application JAR
+ARG JAR_FILE=build/libs/public-cloud-0.0.1-SNAPSHOT.jar
+COPY ${JAR_FILE} app.jar
+
+# 4. Expose port
+EXPOSE 8080
+
+# 5. Run the application
+ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/test/java/com/example/publiccloud/PublicCloudApplicationTests.java
+++ src/test/java/com/example/publiccloud/PublicCloudApplicationTests.java
@@ -2,12 +2,12 @@ package com.example.publiccloud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class PublicCloudApplicationTests {
-
+@ActiveProfiles("test") // 테스트 프로파일 활성화
+public class PublicCloudApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
